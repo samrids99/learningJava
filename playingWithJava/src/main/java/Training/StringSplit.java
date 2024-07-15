eb91ad52d8fc2646f4e015d1bfe5168cb066d5b9
Sam Ridsdale <samridsdale108@gmail.com>
2024-07-15 20:48:28 +0100

    counter and empty array created

--- playingWithJava/src/main/java/Training/StringSplit.java
+++ playingWithJava/src/main/java/Training/StringSplit.java
@@ -5,6 +5,7 @@ public class StringSplit {
     // If the string contains an odd number of characters then it should
     // replace the missing second character of the final pair with an underscore ('_').
     public static String[] solution(String s) {
-        
+        int n = (s.length() + 1) / 2;
+        String[] responses = new String[n];
     }
 }
