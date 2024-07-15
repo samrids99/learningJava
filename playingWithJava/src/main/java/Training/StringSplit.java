153469b5d65d8c0a247e60f24512996c06ad7e59
Sam Ridsdale <samridsdale108@gmail.com>
2024-07-15 20:46:25 +0100

    Task commented in

--- playingWithJava/src/main/java/Training/StringSplit.java
+++ playingWithJava/src/main/java/Training/StringSplit.java
@@ -1,4 +1,7 @@
 package Training;
 
 public class StringSplit {
+    //Complete the solution so that it splits the string into pairs of two characters.
+    // If the string contains an odd number of characters then it should
+    // replace the missing second character of the final pair with an underscore ('_').
 }
