0d5236ef8b7c205a924cf005f206c26f2b5dfb1d
Sam Ridsdale <samridsdale108@gmail.com>
2024-07-17 10:30:40 +0100

    response returned

--- playingWithJava/src/main/java/Training/NarcissisticNumbers.java
+++ playingWithJava/src/main/java/Training/NarcissisticNumbers.java
@@ -5,9 +5,10 @@ public class NarcissisticNumbers {
         int sum = 0;
         String num = Integer.toString(number);
         int length = num.length();
-        for(int i = 0; i < num.length(); i++){
+        for (int i = 0; i < num.length(); i++) {
             int digit = Character.getNumericValue(num.charAt(i));
             sum += (int) Math.pow(digit, length);
         }
+        return sum == number;
     }
 }
