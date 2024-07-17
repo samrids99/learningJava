590acd3a8131f94e0bce3e3adda1679633d47d71
Sam Ridsdale <samridsdale108@gmail.com>
2024-07-17 10:27:47 +0100

    fields initialized

--- playingWithJava/src/main/java/Training/NarcissisticNumbers.java
+++ playingWithJava/src/main/java/Training/NarcissisticNumbers.java
@@ -2,5 +2,8 @@ package Training;
 
 public class NarcissisticNumbers {
     public static boolean isNarcissistic(int number) {
+        int sum = 0;
+        String num = Integer.toString(number);
+        int length = num.length();
     }
 }
